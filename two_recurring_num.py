96316b0a1b745580408207071d3b1097c3e9c050
Olugbenga Okundaye <gbengaokundaye@yahoo.com>
2023-03-28 11:59:00 +0100

    Updated commit

--- two_recurring_num.py
+++ two_recurring_num.py
@@ -1,17 +1,19 @@
 
 
-def find_recurring_char(my_list):
+def first_recurring_char1(my_list):
+    """
+    Shorter and more readable code.
+    with O(n) runtime
+    """
     dic = {}
-    # print(dic)
-    for i in range(len(my_list)):
-        dic[my_list[i]] = my_list[i]
-        if dic[my_list[i]] == my_list[i]:
-            return my_list[i]
-        else:
+    for i in my_list:
+        if i in dic:
             return i
-        # if my_list.count(i) > 1:
-        #     return my_list[i]
+        else:
+            dic[i] = 1
 
 
-# i for i in my_list if my_list.count(i) > 1
-print(find_recurring_char([2, 1, 2, 4, 8, 8, 3, 9, 5, 5]))
+print(first_recurring_char1([2, 1, 2, 4, 8, 8, 3, 9, 5, 5]))
+
+
+
