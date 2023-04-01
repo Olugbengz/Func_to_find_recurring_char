9772cc6816eca56d07d3cb2290e0be89a3fae115
Olugbenga Okundaye <gbengaokundaye@yahoo.com>
2023-04-01 02:42:34 +0100

    Updated commit

--- two_recurring_num.py
+++ two_recurring_num.py
@@ -7,10 +7,12 @@ def first_recurring_char1(my_list):
     """
     dic = {}
     for i in my_list:
+
         if i in dic:
             return i
         else:
             dic[i] = 1
+        print(dic)
 
 
 print(first_recurring_char1([2, 1, 2, 4, 8, 8, 3, 9, 5, 5]))
